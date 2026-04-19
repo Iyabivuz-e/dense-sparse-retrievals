48a6ad91383a9d1fb27a0613a34aae6d1c7fdd3a
Dieudonne Iyabivuze <116635457+Iyabivuz-e@users.noreply.github.com>
2026-04-19 03:35:28 +0200

    implement dense retrievers - hnsw, flat-index

---
+++ models/bge.py
@@ -0,0 +1,9 @@
+
+# from sentence_transformers import SentenceTransformer
+
+# def embed_model(model, documents):
+#     model = SentenceTransformer(model)
+#     embeddings = model.encode(documents, convert_to_numpy=True)
+    
+#     return embeddings 
+
